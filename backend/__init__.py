b5540a44958fc660870f4752c6a8154152265412
Colton Jackson <colton@coltonajackson.com>
2022-05-26 23:34:35 -0400

    Remove __init__.py changes for Django REST API Routing

--- backend/__init__.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import SimpleRouter
-
-class OptionalSlashRouter(SimpleRouter):
-
-    def __init__(self):
-        super().__init__()
-        self.trailing_slash = '/?'
